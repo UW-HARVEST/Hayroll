376c666c7cb461b9ae7afa5237dcd83abba776cb
Haoran Peng <hrpeng@cs.washington.edu>
2025-03-30 05:05:19 +0000

    MacroExpander: add expected error test cases

--- tests/MacroExpander_test.cpp
+++ tests/MacroExpander_test.cpp
@@ -22,11 +22,12 @@ int main(int argc, char **argv)
     TempDir tmpDir;
     std::filesystem::path tmpPath = tmpDir.getPath();
 
-    // List[(List[(defOrUndef)], List[(bench, expected)]]
+    // List[(List[(defOrUndef)], List[(bench, Option[expected])]]
+    // Having no expected value means error is expected
     std::vector<std::tuple
     <
         std::vector<std::string>,
-        std::vector<std::pair<std::string, std::string>>
+        std::vector<std::pair<std::string, std::optional<std::string>>>
     >> testbenches =
     {
         {
@@ -36,7 +37,7 @@ int main(int argc, char **argv)
                 { "defined(A)", "defined ( A )" },
                 { "!A", "! A" },
                 { "!defined(A)", "! defined ( A )" },
-            }
+            },
         },
         {
             {
@@ -47,7 +48,7 @@ int main(int argc, char **argv)
                 { "defined(A)", "1" },
                 { "!A", "!" },
                 { "!defined(A)", "! 1" },
-            }
+            },
         },
         {
             {
@@ -58,7 +59,7 @@ int main(int argc, char **argv)
                 { "defined(A)", "1" },
                 { "!A", "! 1" },
                 { "!defined(A)", "! 1" },
-            }
+            },
         },
         {
             {
@@ -69,7 +70,7 @@ int main(int argc, char **argv)
                 { "defined(A)", "0" },
                 { "!A", "! 0" },
                 { "!defined(A)", "! 0" },
-            }
+            },
         },
         {
             {
@@ -80,12 +81,12 @@ int main(int argc, char **argv)
                 { "defined(A)", "1" },
                 { "!A", "! 1" },
                 { "!defined(A)", "! 1" },
-            }
+            },
         },
         {
             {
                 "#define A 3",
-                "#define F(x) ((x) + 1)"
+                "#define F(x) ((x) + 1)",
             },
             {
                 { "F(1)", "( ( 1 ) + 1 )" },
@@ -93,62 +94,76 @@ int main(int argc, char **argv)
                 { "F(F(F(2)))", "( ( ( ( ( ( 2 ) + 1 ) ) + 1 ) ) + 1 )" },
                 { "F(B) + 1", "( ( B ) + 1 ) + 1" },
                 { "F((x ? x : x))", "( ( ( x ? x : x ) ) + 1 )" },
-            }
+            },
         },
         {
             {
                 "#define F(x) x",
-                "#define Y 1 + F"
+                "#define Y 1 + F",
             },
             {
                 { "Y(2)", "1 + 2" },
-            }
-        },
-        {
-            {
-                "#define F(x) (x + x)",
-                "#define Y F("
             },
-            {
-                { "Y 1)", "( 1 + 1 )" },
-                { "Y 1) + Y 2)", "( 1 + 1 ) + ( 2 + 2 )" },
-            }
         },
         {
             {
                 "#define F(x) (x + x)",
                 "#define Y F(",
-                "#define Z F"
+            },
+            {
+                { "Y 1)", "( 1 + 1 )" },
+                { "Y 1) + Y 2)", "( 1 + 1 ) + ( 2 + 2 )" },
+            },
+        },
+        {
+            {
+                "#define F(x) (x + x)",
+                "#define Y F(",
+                "#define Z F",
             },
             {
                 { "T Y Z (1))", "T ( ( 1 + 1 ) + ( 1 + 1 ) )" },
-            }
+            },
         },
         {
             {
                 "#define G(a, b) a + b",
-                "#define F(x) x(1"
+                "#define F(x) x(1",
             },
             {
-                { "F(G), 2)", "1 + 2" }
-            }
+                { "F(G), 2)", "1 + 2" },
+            },
         },
         {
             {
                 "#define D defined",
-                "#define A D A"
+                "#define A D A",
             },
             {
                 { "A", "1" },
                 { "defined(A)", "1" },
                 { "!A", "! 1" },
                 { "!defined(A)", "! 1" },
-            }
-        }
+            },
+        },
+        {
+            {
+                "#define A A A",
+                "#define F(x) x",
+                "#define G(x) A",
+                "#define H(x) B",
+            },
+            {
+                { "A", std::nullopt },
+                { "F(A)", std::nullopt },
+                { "G(1)", std::nullopt },
+                { "H(A)", std::nullopt }, // Expected false-positive due to implementation limitations
+            },
+        },
     };
 
     // i-th #if -> expected
-    std::vector<std::string_view> testbenchesRef;
+    std::vector<std::optional<std::string_view>> testbenchesRef;
 
     std::string srcString;
     for (const auto & [defs, benches] : testbenches)
@@ -239,22 +254,40 @@ int main(int argc, char **argv)
             TSNode condition = node.childByFieldId(lang.preproc_if_s.condition_f);
             assert(condition.isSymbol(lang.preproc_tokens_s));
             std::vector<TSNode> conditionTokens = lang.tokensToTokenVector(condition);
-            std::vector<Hayroll::TSNode> expanded = expander.expandPreprocTokens(conditionTokens, symbolTable);
+
+            bool isError = false;
+            std::vector<Hayroll::TSNode> expanded;
+            try
+            {
+                expanded = expander.expandPreprocTokens(conditionTokens, symbolTable);
+            }
+            catch (const std::runtime_error & e)
+            {
+                isError = true;
+            }
+            
             std::string expandedStr;
-            for (const TSNode & token : expanded)
+            if (isError)
             {
-                expandedStr += token.text() + " ";
+                expandedStr = "ERROR";
             }
-            // Remove trailing space
-            if (!expandedStr.empty())
+            else
             {
-                expandedStr.pop_back();
+                for (const TSNode & token : expanded)
+                {
+                    expandedStr += token.text() + " ";
+                }
+                // Remove trailing space
+                if (!expandedStr.empty())
+                {
+                    expandedStr.pop_back();
+                }
             }
-            bool isEqual = expandedStr == testbenchesRef[trialId];
-            std::cout << fmt::format("{} expanded: {} -> {}\n", isEqual ? "OK" : "FAIL", condition.text(), expandedStr);
+            bool pass = (!testbenchesRef[trialId] && isError) || (testbenchesRef[trialId] && expandedStr == testbenchesRef[trialId].value());
+            std::cout << fmt::format("{} expanded: {} -> {}\n", pass ? "OK" : "FAIL", condition.text(), expandedStr);
 
             ++trialId;
-            if (!isEqual)
+            if (!pass)
             {
                 allPassed = false;
             }
@@ -262,6 +295,15 @@ int main(int argc, char **argv)
             lastWasIf = true;
         }
         else assert(false);
+    }
+
+    if (!allPassed)
+    {
+        std::cerr << "Some expansions failed\n";
+    }
+    else
+    {
+        std::cout << "All  expansions passed\n";
     }
 
     return allPassed ? 0 : 1;
