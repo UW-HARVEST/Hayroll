6cdbd691f411b399dbf46e448c3282432610bc70
Haoran Peng <hrpeng@cs.washington.edu>
2025-06-04 00:01:17 +0000

    LinemarkerEraser: replaces linemarkers into whitespaces for Maki's convenience

---
+++ src/LinemarkerEraser.hpp
@@ -0,0 +1,42 @@
+// Replaces linemarker text with spaces
+#ifndef HAYROLL_LINEMARKERERASER_HPP
+#define HAYROLL_LINEMARKERERASER_HPP
+
+#include <string>
+
+#include "TreeSitter.hpp"
+#include "TreeSitterCPreproc.hpp"
+#include "ASTBank.hpp"
+#include "TextEditor.hpp"
+
+namespace Hayroll
+{
+
+class LinemarkerEraser
+{
+public:
+    static std::string run(std::string_view text)
+    {
+        TextEditor editor(text);
+        CPreproc lang;
+        ASTBank astBank{lang};
+
+        const Hayroll::TSTree & tree = astBank.addAnonymousSource(std::string(text));
+        TSNode rootNode = tree.rootNode();
+
+        // Iterate over all linemarkers and replace them with spaces
+        for (const TSNode & node : rootNode.iterateDescendants())
+        {
+            if (!node.isSymbol(lang.preproc_line_s)) continue;
+            std::size_t ln = node.startPoint().row + 1; // Convert to 1-based line number
+            std::size_t col = node.startPoint().column + 1; // Convert to 1-based column number
+            std::size_t length = node.length();
+            editor.erase(ln, col, length);
+        }
+        return editor.commit();
+    }
+};
+
+} // namespace Hayroll
+
+#endif // HAYROLL_LINEMARKERERASER_HPP
---
+++ tests/LinemarkerEraser_test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+
+#include "LinemarkerEraser.hpp"
+
+int main(int argc, char **argv)
+{
+    using namespace Hayroll;
+
+    std::filesystem::path srcPath = "../../libmcs/libm/mathf/sinhf.included.c";
+    std::string source = loadFromFile(srcPath);
+    std::string result = LinemarkerEraser::run(source);
+    std::cout << "Source:\n" << source << "\n";
+    std::cout << "Result:\n" << result << "\n";
+
+    // Check if the result is as expected
+
+    CPreproc lang;
+    ASTBank astBank{lang};
+
+    const Hayroll::TSTree & tree = astBank.addAnonymousSource(std::string(result));
+    TSNode rootNode = tree.rootNode();
+
+    // Iterate over all linemarkers and replace them with spaces
+    for (const TSNode & node : rootNode.iterateDescendants())
+    {
+        if (node.isSymbol(lang.preproc_line_s))
+        {
+            std::size_t ln = node.startPoint().row + 1; // Convert to 1-based line number
+            std::size_t col = node.startPoint().column + 1; // Convert to 1-based column number
+            std::size_t length = node.length();
+            std::cout << "Found linemarker at line " << ln << ", column " << col << ", length " << length << "\n";
+            return 1;
+        }
+    }
+    return 0;
+}
