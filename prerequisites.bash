822ea5213f72ad37636b123adf17e1cbc25fdca8
Michael Ernst <mernst@cs.washington.edu>
2025-08-13 21:53:14 -0700

    Remove user interaction

--- prerequisites.bash
+++ prerequisites.bash
@@ -62,11 +62,6 @@ for d in "${THIRD_PARTY_DIRS[@]}"; do
   echo "  - ${INSTALL_DIR}/${d}"
 done
 echo "=========================================================="
-read -rp "Proceed? [y/N] " yn
-[[ "${yn:-N}" =~ ^[Yy]$ ]] || {
-  echo "Aborted."
-  exit 1
-}
 
 mkdir -p "${INSTALL_DIR}"
 cd "${INSTALL_DIR}"
