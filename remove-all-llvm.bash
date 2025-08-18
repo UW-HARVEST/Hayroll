e6b6ec9961568a4b276d0220122a6ab8fb97aa17
Haoran Peng <hrpeng@cs.washington.edu>
2025-08-18 23:54:50 +0000

    Build: standardize usage of SUDO variable in remove-all-llvm.bash

--- remove-all-llvm.bash
+++ remove-all-llvm.bash
@@ -28,15 +28,15 @@ done
 
 export DEBIAN_FRONTEND=noninteractive
 
-"$SUDO" apt update
-"$SUDO" apt remove -y \
+${SUDO} apt update
+${SUDO} apt remove -y \
   clang \
   llvm \
   llvm-dev \
   llvm-runtime:amd64 \
   || true
 for ver in {1..100}; do
-  "$SUDO" apt remove -y \
+  ${SUDO} apt remove -y \
     clang-"${ver}" clang-tools-"${ver}" clangd-"${ver}" lld-"${ver}" \
     libclang-common-"${ver}"-dev libclang-common-"${ver}"-dev:amd64 libclang-cpp"${ver}" \
     llvm-"${ver}" llvm-"${ver}"-dev llvm-"${ver}"-linker-tools llvm-"${ver}"-runtime llvm-"${ver}"-tools \
